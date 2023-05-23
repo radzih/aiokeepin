4ab92c2848a32d6d0ac11e178accb764cbe4a382
Vadim Radzih <iphonevadim2003@gmail.com>
2023-05-23 08:52:50 +0300

    Fixed KeepinStatusError init

--- aiokeepin/exceptions/base.py
+++ aiokeepin/exceptions/base.py
@@ -8,4 +8,4 @@ class KeepinStatusError(KeepinException):
     status_code: int
 
     def __init__(self, *args: object) -> None:
-        self.status_code = args[-2]
+        self.status_code = args[0]
