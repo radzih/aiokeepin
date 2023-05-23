2fb84c6408c391ab06a4fc2a864b5e9b40e3247c
Vadim Radzih <iphonevadim2003@gmail.com>
2023-05-23 18:43:59 +0300

    Test NotFoundError

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,7 +1,11 @@
 import pytest
 
 from aiokeepin import KeepinClient
-from aiokeepin.exceptions import InvalidAPIKeyError, KeepinStatusError
+from aiokeepin.exceptions import (
+    InvalidAPIKeyError,
+    KeepinStatusError,
+    NotFoundError,
+)
 
 
 @pytest.mark.asyncio
@@ -20,3 +24,21 @@ async def test_invalid_api_key_error():
         await client.get("/clients")
     except KeepinStatusError as e:
         assert e.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_not_found():
+    client = KeepinClient("api_key")
+
+    with pytest.raises(NotFoundError):
+        await client.get("/clientssdf")
+
+    try:
+        await client.get("/clientssdf")
+    except NotFoundError as e:
+        assert e.status_code == 404
+
+    try:
+        await client.get("/clientssdf")
+    except KeepinStatusError as e:
+        assert e.status_code == 404
